a185c2c130d4ef62ded301adf7db01268a424bb3
m.frenkel <frenkel.misha@mail.ru>
2019-09-17 14:10:55 +0300

    lab4

---
+++ Lab4/V5_1.sql
@@ -0,0 +1,48 @@
+-- a) Создайте таблицу Sales.CreditCardHst, которая будет хранить информацию об изменениях в таблице Sales.CreditCard.
+/*
+Обязательные поля, которые должны присутствовать в таблице: ID — первичный ключ IDENTITY(1,1);
+ Action — совершенное действие (insert, update или delete); ModifiedDate — дата и время, когда была совершена операция; 
+ SourceID — первичный ключ исходной таблицы; UserName — имя пользователя, совершившего операцию. Создайте другие поля,
+ если считаете их нужными.
+*/
+CREATE TABLE Sales.CreditCardHst (
+	ID INT IDENTITY(1, 1) PRIMARY KEY,
+	Action CHAR(6) NOT NULL CHECK (Action IN('INSERT', 'UPDATE', 'DELETE')),
+	ModifiedDate DATETIME NOT NULL,
+	SourceID INT NOT NULL,
+	UserName VARCHAR(50) NOT NULL
+)
+
+/*
+b) Создайте один AFTER триггер для трех операций INSERT, UPDATE, DELETE для таблицы Sales.CreditCard. 
+Триггер должен заполнять таблицу Sales.CreditCardHst с указанием типа операции в поле Action 
+в зависимости от оператора, вызвавшего триггер.
+*/
+CREATE TRIGGER Sales.CreditCartAfterTrigger
+ON Sales.CreditCard
+AFTER INSERT, UPDATE, DELETE AS
+	INSERT INTO Sales.CreditCardHst (Action, ModifiedDate, SourceID, UserName)
+	SELECT
+		CASE WHEN inserted.CreditCardID IS NULL THEN 'DELETE'
+			 WHEN deleted.CreditCardID IS NULL  THEN 'INSERT'
+												ELSE 'UPDATE'
+		END,
+	GetDate(),
+	COALESCE(inserted.CreditCardID, deleted.CreditCardID),
+	User_Name()
+	FROM inserted FULL OUTER JOIN deleted
+	ON inserted.CreditCardID = deleted.CreditCardID
+
+-- c) Создайте представление VIEW, отображающее все поля таблицы Sales.CreditCard.
+CREATE VIEW Sales.ViewCreditCard AS SELECT * FROM Sales.CreditCard
+
+/*
+d) Вставьте новую строку в Sales.CreditCard через представление. Обновите вставленную строку. 
+Удалите вставленную строку. Убедитесь, что все три операции отображены в Sales.CreditCardHst.
+*/
+INSERT INTO Sales.ViewCreditCard (CardNumber, CardType, ExpMonth, ExpYear, ModifiedDate)
+VALUES (5615465414656541, 'CardType', 10, 2022, GetDate())
+
+UPDATE Sales.CreditCard SET CardType = 'Type' WHERE CardNumber = 5615465414656541
+
+DELETE Sales.CreditCard WHERE CardNumber = 5615465414656541
---
+++ Lab4/V5_2.sql
@@ -0,0 +1,89 @@
+/*
+a) Создайте представление VIEW, отображающее данные из таблиц Sales.CreditCard и Sales.PersonCreditCard. 
+Сделайте невозможным просмотр исходного кода представления. Создайте уникальный кластерный индекс в 
+представлении по полю CreditCardID.
+*/
+CREATE VIEW Sales.ViewExtCreditCard (
+	CreditCardID,
+	CardType,
+	CardNumber,
+	ExpMonth,
+	ExpYear,
+	ModifiedDate,
+	BusinessEntityID,
+	PersonModifiedDate
+) WITH ENCRYPTION, SCHEMABINDING AS SELECT
+	CC.CreditCardID,
+	CC.CardType,
+	CC.CardNumber,
+	CC.ExpMonth,
+	CC.ExpYear,	
+	CC.ModifiedDate,
+	PCC.BusinessEntityID,
+	PCC.ModifiedDate
+FROM Sales.CreditCard AS CC JOIN Sales.PersonCreditCard AS PCC
+ON CC.CreditCardID = PCC.CreditCardID
+
+CREATE UNIQUE CLUSTERED INDEX AK_ViewExtCreditCard_CreditCardID ON Sales.ViewExtCreditCard (CreditCardID)
+
+/*
+b) Создайте три INSTEAD OF триггера для представления на операции INSERT, UPDATE, DELETE. 
+Каждый триггер должен выполнять соответствующие операции в таблицах Sales.CreditCard и Sales.PersonCreditCard 
+для указанного BusinessEntityID. Обновление должно происходить только в таблице Sales.CreditCard. 
+Удаление строк из таблицы Sales.CreditCard производите только в том случае, если удаляемые строки больше не 
+ссылаются на Sales.PersonCreditCard.
+*/
+CREATE TRIGGER Sales.TriggerViewExtCreditCardInsteadInsert ON Sales.ViewExtCreditCard
+INSTEAD OF INSERT AS
+BEGIN
+	INSERT INTO Sales.CreditCard (CardNumber, CardType, ExpMonth, ExpYear, ModifiedDate)
+	SELECT CardNumber, CardType, ExpMonth, ExpYear, ModifiedDate
+	FROM inserted
+	INSERT INTO Sales.PersonCreditCard (BusinessEntityID, CreditCardID, ModifiedDate)
+	SELECT inserted.BusinessEntityID, Sales.CreditCard.CreditCardID, GETDATE()
+	FROM inserted JOIN Sales.CreditCard ON inserted.CardNumber = Sales.CreditCard.CardNumber
+END
+
+CREATE TRIGGER Sales.TriggerViewExtCreditCardInsteadUpdate ON Sales.ViewExtCreditCard
+INSTEAD OF UPDATE AS
+BEGIN
+	UPDATE Sales.CreditCard SET
+		CardNumber = inserted.CardNumber,
+		CardType = inserted.CardType,
+		ExpMonth = inserted.ExpMonth,
+		ExpYear = inserted.ExpYear,
+		ModifiedDate = inserted.ModifiedDate
+	FROM inserted
+	WHERE inserted.CreditCardID = Sales.CreditCard.CreditCardID
+END
+
+CREATE TRIGGER Sales.TriggerViewExtCreditCardInsteadDelete ON Sales.ViewExtCreditCard
+INSTEAD OF DELETE AS
+BEGIN
+	DELETE FROM Sales.PersonCreditCard WHERE BusinessEntityID IN (SELECT BusinessEntityID FROM deleted)
+	DELETE FROM Sales.CreditCard WHERE CreditCardID IN (SELECT CreditCardID FROM deleted) 
+								AND CreditCardID NOT IN (SELECT CreditCardID FROM Sales.PersonCreditCard)
+END
+
+/*
+c) Вставьте новую строку в представление, указав новые данные для CreditCard для существующего
+ BusinessEntityID (например 1). Триггер должен добавить новые строки в таблицы Sales.CreditCard и 
+ Sales.PersonCreditCard. Обновите вставленные строки через представление. Удалите строки.
+*/
+INSERT INTO Sales.ViewExtCreditCard (
+	CardNumber,
+	CardType,
+	ExpMonth,
+	ExpYear,
+	BusinessEntityID,
+	ModifiedDate
+) VALUES ('8976685649', 'ViewCardType', 5, 2021, 1, GetDate())
+
+UPDATE Sales.ViewExtCreditCard SET
+	CardNumber = '132456',
+	CardType = 'Default',
+	ExpMonth = 4,
+	ExpYear = 2022
+WHERE CardNumber = '8976685649'
+
+DELETE FROM Sales.ViewExtCreditCard WHERE CardNumber = '132456'
